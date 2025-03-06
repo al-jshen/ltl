fe9ffb6a064600848028e182ee510d6d25ac4d2e
Jeff Shen <jshen@flatironinstitute.org>
2025-03-06 14:27:32 -0500

    add flow policy

--- ltl/policies.py
+++ ltl/policies.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+from .flow import RealNVP
+
 
 class CategoricalPolicy(nn.Module):
     def __init__(self, obs_dim, action_dim, hidden_dim):
@@ -27,5 +29,26 @@ class CategoricalPolicy(nn.Module):
         return action.detach().cpu().numpy(), log_prob
 
 
+class FlowPolicy(nn.Module):
+    def __init__(self, obs_dim, action_dim, hidden_dim, cond_dim):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(obs_dim, hidden_dim),
+            nn.GELU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.GELU(),
+            nn.Linear(hidden_dim, cond_dim),
+        )
+        self.flow = RealNVP(data_dim=action_dim, context_dim=cond_dim, activation="GELU", hidden_dims=[hidden_dim, hidden_dim])
+
+    def forward(self, x):
+        x = torch.tensor(x, dtype=torch.float32, device=next(self.parameters()).device)
+        conditioner = self.net(x)
+        return conditioner
+
+    def sample(self, conditioner):
+        action = self.flow.sample(1, context=conditioner).squeeze(0)
+        log_prob = self.flow.log_prob(action, context=conditioner)
+        return action.detach().cpu().numpy(), log_prob
 
 
